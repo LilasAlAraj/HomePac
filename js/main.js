e0db8e61d73a2f29df754e6bbc16f201cc638ad2
LilasAlAraj <lilas.alaraj.9@gmail.com>
2023-08-08 02:39:39 +0300

    final touches

---
+++ js/main.js
@@ -0,0 +1,9 @@
+ // Create a new Date object
+ const currentDate = new Date();
+ // Get the current year
+ const currentYear = currentDate.getFullYear();
+ years= document.getElementsByClassName('year')
+for (var i=0; i<years.length; i++) {
+    years[i].innerHTML = currentYear;
+}
+
